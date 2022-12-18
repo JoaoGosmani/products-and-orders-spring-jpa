b5204d0a48984b55cbbcd6efdd8da3f658e2db1d
Joao Gosmani <joaogosmani@gmail.com>
2022-12-17 22:59:10 -0300

    Project created

---
+++ src/main/java/br/com/joaogosmani/productsandorders/ProductsAndOrdersApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.productsandorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsAndOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsAndOrdersApplication.class, args);
+	}
+
+}
