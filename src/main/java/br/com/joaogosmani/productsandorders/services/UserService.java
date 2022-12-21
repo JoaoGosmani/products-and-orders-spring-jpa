5f94ff6d53cd3205a3622c388a49a0e4764f5079
Joao Gosmani <joaogosmani@gmail.com>
2022-12-20 23:33:19 -0300

    Exception handling - findById

--- src/main/java/br/com/joaogosmani/productsandorders/services/UserService.java
+++ src/main/java/br/com/joaogosmani/productsandorders/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.joaogosmani.productsandorders.entities.User;
 import br.com.joaogosmani.productsandorders.repositories.UserRepository;
+import br.com.joaogosmani.productsandorders.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> user = repository.findById(id);
-		return user.get();
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
